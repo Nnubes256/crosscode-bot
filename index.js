75e2da3ebf5d15047a3f2c05a52be8a126394310
ac2pic <ac2pic@gmail.com>
2017-11-17 21:58:40 -0600

    Updated help

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-let {readFileSync} = require('fs');
-let prefix = ".cc";
+let {
+    readFileSync
+} = require('fs');
+let prefix = process.env.BOT_PREFIX;
 let generalCommands = require('./js/command/general/commands.js')
 let nsfwCommands = require('./js/command/nsfw/commands.js')
 client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}!`);
-	//client.user.setAvatar('avatar/manlea.png')
+    console.log(`Logged in as ${client.user.tag}!`);
+    //client.user.setAvatar('avatar/manlea.png')
 });
-function end() {
-	client.destroy()
-}
-function onMessage(msg){
-	let args = msg.content.split(' ');
-	let _prefix = args.shift();
-	if(_prefix !== prefix)
-		return;
-	console.log(args)
-	let type = args.shift();
-	//user wants a general command
-	if(type === '-g') {
-		let command = args.shift();
-		let func = generalCommands[command] || generalCommands.error
-		func(msg, command, args, client)
-	}
-	else if(type === "-nsfw") {
-		let command = args.shift()
-		let func = nsfwCommands[command]
-		try {
-			func(msg, command, args, console)
-		}catch(e) {
-			console.log(e)
-		}
-	}
+
+function onMessage(msg) {
+    let args = msg.content.split(' ');
+    let _prefix = args.shift();
+    if (_prefix !== prefix)
+        return;
+    let type = args.shift();
+    //user wants a general command
+    if (type === '-g') {
+        let command = args.shift();
+        let func = generalCommands[command] || generalCommands.error
+        func(msg, command, args, client)
+    } else if (type === "-nsfw") {
+        let command = args.shift()
+        let func = nsfwCommands[command]
+        try {
+            func(msg, command, args, console)
+        } catch (e) {
+            console.log(e)
+        }
+    }
 }
 client.on('message', onMessage);
 client.login(process.env.BOT_TOKEN);
--- js/discord-util.js
+++ js/discord-util.js
@@ -1,24 +1,26 @@
 const Discord = require('discord.js')
 exports.getEmoji = function(object, name) {
     let emojis = null
-    if(object instanceof Discord.Message) {
+    if (object instanceof Discord.Message) {
         emojis = object.channel.guild.emojis
     }
-     return emojis.find("name", name) || `:${name}:`
+    return emojis.find("name", name) || `:${name}:`
 }
+
 function filterUserId(id) {
-	return id.replace(/[^0-9]/g, "")
+    return id.replace(/[^0-9]/g, "")
 }
+
 function isId(id) {
-	return (id.startsWith("<@") || id.startsWith("<@!")) && id.endsWith(">")
+    return (id.startsWith("<@") || id.startsWith("<@!")) && id.endsWith(">")
 }
 exports.findMember = function(object, string) {
     let member = null;
-    if(object instanceof Discord.Message) {
-		if(isId(string)) {
-			string = filterUserId(string)
-		}
-        member =  object.channel.guild.members.find(function(item) {
+    if (object instanceof Discord.Message) {
+        if (isId(string)) {
+            string = filterUserId(string)
+        }
+        member = object.channel.guild.members.find(function(item) {
             return item.user.username.indexOf(string) > -1 || item.user.id.indexOf(string) > -1
         })
     }
