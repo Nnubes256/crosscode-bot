8cde37bb3c003ab4af7e04188188fd4cb696ab3a
ac2pic <ac2pic@gmail.com>
2017-11-19 13:16:43 -0600

    Reorganized commands

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ let {
     readFileSync
 } = require('fs');
 let prefix = process.env.BOT_PREFIX;
-let generalCommands = require('./js/command/general/commands.js')(client)
-let nsfwCommands = require('./js/command/nsfw/commands.js')(client)
+let commands = {
+    "": require('./js/command/general/commands.js')(client),
+    "nsfw": require('./js/command/nsfw/commands.js')(client),
+    "voice": require('./js/command/voice/commands.js')(client)
+}
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     //client.user.setAvatar('avatar/cloudlea.png')
@@ -16,19 +19,17 @@ function onMessage(msg) {
         return;
     let args = msg.content.split(' ');
     let _prefix = args.shift();
-    if (_prefix !== prefix)
+    if (!_prefix.startsWith(prefix))
         return;
-    let type = args.shift();
-    //user wants a general command
-    if (type === "-nsfw") {
-        let command = args.shift()
-        let func = nsfwCommands[command]
-        func(msg, args, command, console)
-    } else {
-        let command = type;
-        let func = generalCommands[command] || generalCommands.error
-        func(msg, args, command)
+    let type = _prefix.substring(1)
+    let commandType = commands[type]
+    if (!commandType) {
+        generalCommands.error(msg, args, command)
+        return;
     }
+    let command = args.shift()
+    let func = commandType[command]
+    func(msg, args, command, console)
 }
 client.on('message', onMessage);
 client.login(process.env.BOT_TOKEN);
--- js/command/general/commands.js
+++ js/command/general/commands.js
@@ -44,45 +44,6 @@ let generalCommands = function(instance) {
         sleep: function sleep() {
             instance.destroy();
         },
-        joinvoice: function joinVoiceChannel(msg) {
-            if (msg.member.voiceChannel) {
-                msg.member.voiceChannel.join().then(function(success) {
-                    msg.reply('I joined.');
-                }).catch(function(error) {
-                    msg.reply(`${error}`);
-                })
-            } else {
-                msg.reply('you are not in a voice channel.');
-            }
-        },
-        play: function playMusic(msg, args) {
-            if (msg.member.voiceChannel) {
-                let voiceChannel = instance.channels.find("id", msg.member.voiceChannel.id);
-                if (voiceChannel) {
-                    try {
-                        voiceChannel.connection.playFile('./music/' + args.join(" ") + '.mp3');
-                    } catch (e) {
-                        console.log(e)
-                    }
-                    msg.reply('am I playing music?')
-                } else {
-                    msg.reply("not in your voice channel.")
-                }
-            } else {
-                msg.reply('dude. You have to be in a voice chat.')
-            }
-        },
-        leavevoice: function stopMusic(msg) {
-            let voiceConnection = instance.channels.findAll("type", "voice").find(function(channel) {
-                return channel.guild.id === msg.guild.id;
-            })
-            if (voiceConnection) {
-                voiceConnection.leave()
-                msg.reply("left the voice channel!");
-            } else {
-                msg.reply("but I'm not in a voice channel!");
-            }
-        },
         hug: function hugUser(msg) {
             console.log('todo .cc -g hug')
         },
