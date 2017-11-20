3a3c7eff057d517c04d0755d15efcc37f5562ee1
ac2pic <ac2pic@gmail.com>
2017-11-19 21:31:53 -0600

    Changed lea glasses to HI!

--- js/command/general/commands.js
+++ js/command/general/commands.js
@@ -105,11 +105,10 @@ let generalCommands = function(instance) {
                 image: 'https://cdn.discordapp.com/attachments/376138665954377728/381565961582411777/Lea_triggered.png'
             }))
         },
-        lea: function(msg, args, command) {
-            if (args[0] === "glasses")
-                msg.channel.send(createRichEmbed({
-                    image: 'https://cdn.discordapp.com/attachments/373163281755537420/381790329550143488/Deal_with_it_Lea.gif'
-                }))
+        "HI!": function(msg, args, command) {
+            msg.channel.send(createRichEmbed({
+                image: 'https://cdn.discordapp.com/attachments/373163281755537420/381790329550143488/Deal_with_it_Lea.gif'
+            }))
         }
     }
 };
