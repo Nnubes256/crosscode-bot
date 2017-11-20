5a6a8f9b14e12892af37eacace12f5e4c3b0c425
ac2pic <ac2pic@gmail.com>
2017-11-19 22:24:14 -0600

    Needed to change main for it to work

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let {
 } = require('fs');
 let prefix = process.env.BOT_PREFIX;
 let commands = {
-    "": require('./js/command/general/commands.js')(client),
+    "g": require('./js/command/general/commands.js')(client),
     "nsfw": require('./js/command/nsfw/commands.js')(client),
     "voice": require('./js/command/voice/commands.js')(client),
     "mods": require('./js/command/mods/commands.js')(client)
