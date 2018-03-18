e4a318b36e49ee1a58c74dee377384c1e3a9a8cd
Alwinfy <wenming.yi@gmail.com>
2018-03-18 18:50:36 -0500

    implement argparse

--- discord-util.js
+++ discord-util.js
@@ -247,3 +247,30 @@ exports.greetingsParse = function(guild, msg) {
    }
    return msg;
 }
+function argParse(str) {
+    let spl = [''], esc = false, quot = true;
+    for (let c of str) {
+        if (esc) { // last character was a backslash, skip handling
+            esc = false;
+            spl[spl.length - 1] += '\\' + c;
+            continue;
+        }
+        switch(c) {
+        case '\\':
+            esc = true; // escape next character
+            break;
+        case '"':
+            quot = !quot;
+            break;
+        case ' ':
+        case '\t':
+            if (quot) {
+                spl.push(''); // split on unquoted spaces
+                break;
+            }
+        default:
+            spl[spl.length - 1] += c;
+        }
+    }
+    return spl;
+}
