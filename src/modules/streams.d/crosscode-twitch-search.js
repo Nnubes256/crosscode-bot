ed44181a630b6e785c4121f64e67bfe1835e8a8f
2767Mr <2767mr@gmx.at>
2018-03-27 19:31:37 +0200

    Reworked streams module

---
+++ src/modules/streams.d/crosscode-twitch-search.js
@@ -0,0 +1,117 @@
+const rp = require('request-promise');
+
+const { Utils } = require('../../utils');
+const { Env } = require('../../env');
+
+class CrossCodeStream {
+    constructor() {
+        this.getGameId()
+            .then(() => {
+                console.log("CrossCode id:", this.id);
+                console.log("Updating list");
+                this.updateList();
+            })
+            .catch(err => console.error(err));
+        setInterval(this.updateList.bind(this), (1 * 5 * 60 * 1000));
+    }
+    get() {
+        return this.list;
+    }
+    getGameId() {
+        return this.makeRequest({
+            uri: 'https://api.twitch.tv/helix/games?name=CrossCode'
+        }).then(response => {
+            /** @type {string} */
+            this.id = response.data[0].id;
+        });
+    }
+    createRequest(opts) {
+        const options = {
+            headers: {
+                'Client-ID': Env.TWITCH_CLIENT_ID
+            },
+            json: true
+        };
+        for (let keys in opts) {
+            if (keys !== "headers") {
+                options[keys] = opts[keys];
+            }
+        }
+        if (typeof opts.headers === "object") {
+            for (let header_value in opts.headers) {
+                options.headers[header_value] = opts.headers[header_value];
+            }
+        }
+        return options;
+    }
+    makeRequest(opts) {
+        return rp(this.createRequest(opts));
+    }
+    /**
+     * 
+     * @param {string[]} ids_arr 
+     */
+    createIdStream(ids_arr) {
+        /** @type {string[]} */
+        const copy = JSON.parse(JSON.stringify(ids_arr))
+        copy.forEach((id, index, arr) => {
+            arr[index] = "id=" + id;
+        });
+        return copy.join("&");
+    }
+    /**
+     * 
+     * @param {{name: string, value: string}[]} stream_list 
+     */
+    generateList(stream_list) {
+        if (!stream_list.length) {
+            this.list = null;
+            return;
+        }
+        //TODO: Finish this
+
+        this.list = Utils.createRichEmbed({
+            title: "CrossCode Twitch Streams",
+            fields: stream_list,
+            timestamp: new Date()
+        });
+    }
+    updateList() {
+        if (!this.id) {
+            console.log("Could not find id");
+            return;
+        }
+        let streamData = [];
+        this.makeRequest({
+            uri: `https://api.twitch.tv/helix/streams?type=live&game_id=${this.id}`
+        }).then(res => {
+            streamData = res.data;
+            //TODO: Handle when no streamers
+            /** @type {string[]} */
+            const streamerIds = res.data.map(streamer => streamer.user_id);
+            const ids_list = this.createIdStream(streamerIds);
+            if (ids_list.length) {
+                return this.makeRequest({
+                    uri: `https://api.twitch.tv/helix/users?${ids_list}`
+                });
+            }
+            this.list = null;
+
+        }).then(res => {
+            //we will assume the result is in the order that they came in...
+            const users = res.data;
+            /** @type {{name: string, value: string}[]} */
+            const streams_list = [];
+            streamData.forEach((stream, index) => {
+                const user = users[index];
+                streams_list.push({
+                    name: `${stream.title} by ${user.display_name}`,
+                    value: `Stream language: ${stream.language}\n[Join Stream](https://www.twitch.tv/${user.login})`
+                });
+            });
+            this.generateList(streams_list);
+        }).catch(err => console.error(err));
+
+    }
+}
+exports.CrossCodeStream = CrossCodeStream
--- src/modules/streams.js
+++ src/modules/streams.js
@@ -1,7 +1,63 @@
+const { Message, TextChannel } = require('discord.js');
+
 const { Module } = require('../module');
+const { Utils } = require('../utils');
+const { CrossCodeStream } = require('./streams.d/crosscode-twitch-search');
 
 class Streams extends Module{
+    constructor() {
+        super();
 
+        this.streams = new CrossCodeStream();
+        /** @type {TextChannel[]} */
+        this.notify_channels = [];
+
+        setInterval(() => {
+            const streamEmbed = this.streams.get();
+            //console.log("Showing streams");
+            if (!streamEmbed) {
+                //console.log("None found, aborting");
+                return;
+            }
+            //console.log("Found!");
+            for (let channel of this.notify_channels) {
+                if (channel)
+                    channel.send('', streamEmbed);
+            }
+        }, (30 * 60 * 1000)); //Updates channels every 30 minutes
+    }
+
+    getCommands() {
+        return {
+            /**
+             * @param {Message} msg
+             */
+            set: msg => {
+                if (!Utils.isAdmin(msg.member))
+                    return;
+                
+                this.notify_channels.push(msg.channel);
+                msg.channel.send('This channel has been set to be notified of CrossCode streams periodically.');
+            },
+            /**
+             * @param {Message} msg
+             */
+            get: msg => {
+                const sEmbed = this.streams.get();
+                msg.channel.send("Streaming CrossCode right now:" + (sEmbed ? '' : "\n*Tumbleweeds rolling*"), sEmbed);
+            },
+            /**
+             * @param {Message} msg
+             */
+            remove: msg => {
+                if (!Utils.isAdmin(msg.member))
+                    return;
+                    
+                delete this.notify_channels[this.notify_channels.indexOf(msg.channel)];
+                msg.channel.send('This channel will no longer be notified of streams');
+            }
+        }
+    }
 }
 
 exports.streams = new Streams();
