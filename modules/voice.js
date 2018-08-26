49be8b2e715cd1489b71c2c1c93abbdbb9e39416
Alwinfy <20421383+Alwinfy@users.noreply.github.com>
2018-08-26 15:00:47 -0500

    clean up voice module

--- modules/voice.js
+++ modules/voice.js
@@ -1,5 +1,4 @@
-var currentlyConnectedToChannel = false;
-var currentVoiceChannel = null;
+const voiceChans = {};
 
 var greetTracks = [
     "hi", "hi-2", "hi-3", "haaaaii", "haaaaii-2"
@@ -13,96 +12,97 @@ var leaTracks = [
     "lea", "lea-2", "lea-3"
 ];
 
+const playVoice = (tracks, vc) => vc.connection.playFile('./music/voice/' + tracks[Math.floor(Math.random()*tracks.length)] + '.ogg', {volume: 4});
+
 module.exports = function(instance) {
     instance.on('voiceStateUpdate', (oldM, newM) => {
-        if(oldM.voiceChannel === undefined && newM.voiceChannel !== undefined) {
+        const vc = voiceChans[newM.guild.id];
+        if (!vc) return;
+        if (!oldM.voiceChannel && newM.voiceChannel) {
             // Join
-            if (!currentVoiceChannel) return;
-            if (newM.voiceChannel.id === currentVoiceChannel.channel.id) {
-                setTimeout(() => {
-                    currentVoiceChannel.playFile('./music/voice/' + greetTracks[Math.floor(Math.random()*greetTracks.length)] + '.ogg', {volume: 4});
-                }, 500);
+            if (newM.voiceChannelID === vc.id) {
+                setTimeout(() => playVoice(greetTracks, vc), 500);
             }
-        } else if(newM.voiceChannel === undefined){
+        } else if (oldM.voiceChannel && !newM.voiceChannel){
             // Leave
-            if (!currentVoiceChannel) return;
-            if (oldM.voiceChannel.id === currentVoiceChannel.channel.id) {
-                currentVoiceChannel.playFile('./music/voice/' + leaveTracks[Math.floor(Math.random()*leaveTracks.length)] + '.ogg', {volume: 4});
+            if (oldM.voiceChannelID === vc.id) {
+                playVoice(leaveTracks, vc);
             }
         }
     });
 
     instance.on('message', (msg) => {
-        if (!currentVoiceChannel) return;
         if (
+            msg.guild &&
             msg.isMentioned(instance.user) &&
             /wh?at(\s+i|')[sz]\s+y(ou|[ae])r?\s+nae?me?/i.test(msg.content) &&
-            msg.member.voiceChannel.id === currentVoiceChannel.channel.id
+            msg.member.voiceChannelID === voiceChans[msg.guild.id].id
         ) {
-            currentVoiceChannel.playFile('./music/voice/' + leaTracks[Math.floor(Math.random()*leaTracks.length)] + '.ogg', {volume: 4});
+            playVoice(leaTracks, vc);
         }
     });
 
     let commands = {
         join: function joinVoiceChannel(msg) {
-            if (msg.member.voiceChannel && !currentlyConnectedToChannel) {
-                msg.member.voiceChannel.join().then((connection) => {
-                    currentVoiceChannel = connection;
-                    msg.reply('I joined.');
-
-                    setTimeout(() => {
-                        currentVoiceChannel.playFile('./music/voice/' + greetTracks[Math.floor(Math.random()*greetTracks.length)] + '.ogg', {volume: 4});
-                    }, 300);
-                }).catch(function(error) {
-                    msg.reply(`${error}`);
-                });
-
-                currentlyConnectedToChannel = true;
-            } else {
-                msg.reply('you are not in a voice channel.');
+            if (!msg.guild) return;
+            let vc = voiceChans[msg.guild.id];
+            if (!msg.member.voiceChannel) {
+                msg.reply("you aren't in a voice channel!");
+                return;
             }
+            if (vc) {
+                msg.reply("I'm already in a voice channel!");
+                return;
+            }
+            msg.member.voiceChannel.join().then((connection) => {
+                vc = voiceChans[msg.guild.id] = msg.member.voiceChannel;
+		console.log(vc);
+                msg.reply('I joined.');
+
+                setTimeout(() => playVoice(greetTracks, vc), 300);
+            }).catch(function(error) {
+                msg.reply(`${error}`);
+            });
+
         },
         play: function playMusic(msg, args) {
-            if (msg.member.voiceChannel) {
-                let voiceChannel = msg.member.voiceChannel.members.find(function(member) {
-                    return member.user.id === instance.user.id;
-                });
-                if (voiceChannel) {
-                    var name = args.join(" ");
-                    if(name.indexOf("http") > -1) {
-                      msg.member.voiceChannel.connection.playFile(name);
-                      msg.reply('am I playing music?');
-                    } else {
-                      try {
-                          msg.member.voiceChannel.connection.playFile('./music/' + name + '.mp3');
-                          msg.reply('am I playing music?');
-                      } catch (e) {
-                          console.log(e)
-                      }
-                    }
-
-
-                } else {
-                    msg.reply("not in your voice channel.")
-                }
+            if (!msg.guild) return;
+            const vc = voiceChans[msg.guild.id];
+            if (!vc) {
+                msg.reply("I'm not in a voice channel!");
+                return;
+            }
+            if (msg.member.voiceChannelID !== vc.id) {
+                msg.reply("you're not in the voice channel I'm in!");
+                return;
+            }
+            var name = args.join(" ");
+            if (name.indexOf("http") > -1) {
+                vc.connection.playFile(name);
+                msg.reply('am I playing music?');
             } else {
-                msg.reply('dude. You have to be in a voice chat.')
+                try {
+                    vc.connection.playFile('./music/' + name + '.mp3');
+                    msg.reply('am I playing music?');
+                } catch (e) {
+                    console.log(e)
+                }
             }
         },
         leave: function stopMusic(msg) {
-            let voiceConnection = instance.channels.findAll("type", "voice").find(function(channel) {
-                return channel.guild.id === msg.guild.id;
-            })
-            if (voiceConnection) {
-                currentVoiceChannel.playFile('./music/voice/' + leaveTracks[Math.floor(Math.random()*leaveTracks.length)] + '.ogg', {volume: 4});
-                setTimeout(() => {
-                    voiceConnection.leave();
-                    msg.reply("left the voice channel!");
-                    currentlyConnectedToChannel = false;
-                }, 1200);
-            } else {
-                msg.reply("but I'm not in a voice channel!");
+            if (!msg.guild) return;
+            const vc = voiceChans[msg.guild.id];
+            if (!vc) {
+                msg.reply("I'm not in a voice channel!");
+                return;
             }
+            
+            playVoice(leaveTracks, vc);
+            setTimeout(() => {
+                vc.leave();
+                delete voiceChans[msg.guild.id];
+                msg.reply("left the voice channel!");
+            }, 1200);
         },
     };
     return commands;
